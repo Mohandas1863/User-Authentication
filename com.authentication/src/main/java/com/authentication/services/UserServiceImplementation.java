78399fe16ca66cf2f402b8def88c7e49a73f7fa0
Mohandas1863 <mohandasbiradar@gmail.com>
2024-12-06 21:22:53 +0530

    Add files via upload

---
+++ com.authentication/src/main/java/com/authentication/services/UserServiceImplementation.java
@@ -0,0 +1,40 @@
+package com.authentication.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.authentication.entities.User;
+import com.authentication.repositories.UserRepository;
+
+@Service
+public class UserServiceImplementation implements UserService {
+
+	@Autowired
+	UserRepository repo;
+
+	public boolean usernameExists(String username) {
+		User user = repo.findByUsername(username);
+		if (user != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void addUser(User user) {
+		repo.save(user);
+	}
+
+	@Override
+	public boolean validateUser(String username, String password) {
+		if (usernameExists(username)) {
+			User user = repo.findByUsername(username);
+			String dbPass = user.getPassword();
+			if (password.equals(dbPass))
+				return true;
+		}
+
+		return false;
+	}
+}
